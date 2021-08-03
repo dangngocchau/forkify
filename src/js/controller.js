8d593b4306929d5c4f9c8e8281414624b9926cf4
Chou <dnchau2310@gmail.com>
2021-08-03 17:01:14 +0700

    Test netlify

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,7 +118,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-
+const testNetlify = function () {
+  console.log('TEST');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -127,5 +129,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  testNetlify();
 };
 init();
