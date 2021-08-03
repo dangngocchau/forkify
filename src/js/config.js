b2c8285a1c2f82a6572b5c3e5f35d301d99d886e
Chou <dnchau2310@gmail.com>
2021-08-03 09:49:50 +0700

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd86c0a0d-edc4-4614-8fad-01272c4050f3';
+export const MODAL_CLOSE_SEC = 2.5;
